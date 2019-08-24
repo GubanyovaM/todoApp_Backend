ff99ccc6e18a41862d2fbe7cbfeebd53ad3347b4
unknown <gubanyova.maria@gmail.com>
2019-08-24 19:21:03 +0200

    profanity validator 2 versions

--- src/main/java/com/accenture/letovit/todolist/validator/ToDoItemValidator.java
+++ src/main/java/com/accenture/letovit/todolist/validator/ToDoItemValidator.java
@@ -1,5 +1,7 @@
 package com.accenture.letovit.todolist.validator;
 
+import java.util.ArrayList;
+
 import com.accenture.letovit.todolist.ToDoItem;
 
 public class ToDoItemValidator {
@@ -18,6 +20,29 @@ public class ToDoItemValidator {
 				throw new RuntimeException("znak nie je ani cislo ani pismeno!!!!!");
 			}
 		}
+		
+		// profanity validator ver.1 
+		String[] profanityWords = {"fuck", "buck", "duck", "fff", "ggg"};
+		for (String word : profanityWords) {
+		
+			if (toDoItem.getTitle().contains(word) || toDoItem.getText().contains(word)) {
+				throw new RuntimeException("škaredé slovo!! " + word +" no no!!");
+			}
+		}
+		
+		// profanity validator ver.2
+		ArrayList<String> profanityWords2 = new ArrayList<String>();
+			profanityWords2.add("nnn"); 
+			profanityWords2.add("aaa"); 
+			profanityWords2.add("eee");
+			
+			profanityWords2.forEach(word2 -> {
+				if (toDoItem.getTitle().contains(word2) || toDoItem.getText().contains(word2)) {
+					throw new RuntimeException("škaredé slovo!! " + word2 + " no no!!");
+				}
+				
+			});
+		
 	}
 	
 
